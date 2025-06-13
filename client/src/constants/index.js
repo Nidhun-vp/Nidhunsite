921d4a799074618ae5315153aa9d7ad6257ac55e
NidhuN <100463732+Nidhun-vp@users.noreply.github.com>
2025-06-13 12:06:48 +0530

    Update index.js

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -4,7 +4,7 @@ const olxClone = "https://res.cloudinary.com/duyhgge54/image/upload/v1700000000/
 const food = "https://res.cloudinary.com/duyhgge54/image/upload/v1700000000/food_nmrt0x";
 const TodoList = "https://res.cloudinary.com/duyhgge54/image/upload/v1700000000/todoapp_hikg39";
 const airport = "https://res.cloudinary.com/duyhgge54/image/upload/v1700000000/airport_u47oxb";
-const Job= "https://res.cloudinary.com/duyhgge54/image/upload/v1749795747/job-board_nuatwf.jpg"
+const Job= "https://res.cloudinary.com/duyhgge54/image/upload/v1749795747/job-board_nuatwf"
 
 
 
